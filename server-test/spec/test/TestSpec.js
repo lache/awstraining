e3e2133f983dd7f67b21964b290ceff1e198c426
Geoyeob Kim <gasbank@msn.com>
2015-09-15 22:09:22 +0900

    패키지 분리

---
+++ server-test/spec/test/TestSpec.js
@@ -0,0 +1,7 @@
+describe('CharlieCoreTest', function() {
+    it('Test 1', function() {
+        var charlie = require('charlie-core');
+        //var board = new CHARLIE.ataxx.Board();
+        //expect(board).toBeDefined();
+    });
+});
