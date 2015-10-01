c97161d0dae2e56ffa48f5a4d963e6420b9e88ed
Geoyeob Kim <gasbank@msn.com>
2015-10-01 16:21:41 +0900

    match-3 플레이~

--- clientjs/src/Match3Scene.js
+++ clientjs/src/Match3Scene.js
@@ -28,6 +28,10 @@ var Match3Scene = cc.Scene.extend({
         // this.addChild(this.sprite);
         this.cells = [];
 
+        this.cellInstances = {};
+
+        this.precreateCells();
+
         this.refreshAllCells();
 
         cc.eventManager.addListener({
@@ -40,19 +44,56 @@ var Match3Scene = cc.Scene.extend({
                 this.stepAndRefresh();
             }).bind(this)
         }, this);
+
+        this.startGame();
+    },
+    precreateCells: function() {
+        for (let type = 1; type <= 7; type++) {
+            this.cellInstances[type] = [];
+            for (let i = 0; i < 7*7; i++) {
+                let cell = this.createNewCell(type);
+                cell.type = type;
+                cell.retain();
+                this.cellInstances[type].push(cell);
+            }
+        }
+    },
+    startGame: function() {
+        this.stepAndRefreshUntilStable();
+    },
+    isStable: function() {
+        let notStable = (this.board.getTurn() == 0 && this.board.getStep() == 0) ||
+            (this.board.getStep() != 0) ||
+            this.moved;
+
+        return !notStable;
+    },
+    stepAndRefreshUntilStable: function() {
+        cc.log('unstable?');
+
+        if (!this.isStable()) {
+            this.moved = false;
+
+            this.stepAndRefresh();
+
+            if (!this.isStable()) {
+                this.runAction(cc.sequence(cc.delayTime(0.1),
+                    cc.callFunc(this.stepAndRefreshUntilStable, this)));
+            }
+        }
     },
     stepAndRefresh: function() {
         this.board.nextStep();
         cc.log('Next step - TURN: ' + this.board.getTurn() +
             '    STEP: ' + this.board.getStep());
 
-        //var label = event.getCurrentTarget();
-        //label.setString("Key " + (cc.sys.isNative ? that.getNativeKeyName(keyCode) : String.fromCharCode(keyCode) ) + "(" + keyCode.toString()  + ") was released!");
         this.refreshAllCells();
     },
     refreshAllCells: function() {
-        for (let i  = 0; i < this.cells.length; i++) {
-            this.cells[i].removeFromParent();
+        for (let i = 0; i < this.cells.length; i++) {
+            let cellToBeRemoved = this.cells[i];
+            cellToBeRemoved.removeFromParent();
+            this.cellInstances[cellToBeRemoved.type].push(cellToBeRemoved);
         }
 
         this.cells = [];
@@ -74,7 +115,7 @@ var Match3Scene = cc.Scene.extend({
         board.setSize(width, height);
         let entireBoard = this.createRandomEntireBoard(width, height);
         board.setEntireBoard(entireBoard);
-        board.setNewCells(this.createRandomNewCells(100));
+        board.setNewCells(this.createRandomNewCells(1000));
         return board;
     },
     random: function(a, b) {
@@ -101,90 +142,116 @@ var Match3Scene = cc.Scene.extend({
         var cellCountYHalf = Math.floor(cellCountY / 2);
         let ix = x;
         let iy = y;
-        // TODO 플레이어는 무조건 빨간색?
-        var f = 'res/gem' + type + '.json';
-        let cell = ccs.load(f).node;
-        cell.attr({
-            x: this.boardX0 + (ix - cellCountXHalf) * this.dCellWidth,
-            y: this.boardY0 + (cellCountYHalf - iy) * this.dCellHeight
-        });
+
+        //let cell = this.createNewCell(type);
+        //cc.log('processPlaceCmd - type=' + type);
+        let cell = this.cellInstances[type].pop();
+
         cell.ix = ix;
         cell.iy = iy;
+        cell.attr({
+            x: this.boardX0 + (ix - cellCountXHalf) * this.dCellWidth,
+            y: this.boardY0 + (cellCountYHalf - iy) * this.dCellHeight,
+            //scale: 0.7,
+        });
+
+        this.addChild(cell);
+        return cell;
+    },
+    createNewCell: function(type) {
+        var f = 'res/gem' + type + '.json';
+        //let cell = new ccui.ImageView('#gem/gem' + type + '.png');
+        //let cell = new ccui.ImageView('res/back1.png');
+        //let cell = new ccui.ImageView('#gem/gem' + type + '.png');
+
+        //let cell = new ccui.Button();
+        //cell.loadTextures('#gem/gem' + type + '.png', '#gem/gem' + type + '.png', '#gem/gem' + type + '.png');
+
+        let cell = ccs.load(f).node;
         cell.getChildByTag(294).addTouchEventListener(
             this.cellTouchEventListener, cell);
+
+        //cell.addTouchEventListener(this.cellTouchEventListener, cell);
+
         cell.moveTo = (function(dx, dy) {
-            console.log('(' + ix + ',' + iy + ') -> (' + (ix + dx) + ',' + (iy + dy) + ')');
-            return this.board.move(ix, iy, ix + dx, iy + dy);
+            console.log('(' + cell.ix + ',' + cell.iy + ') -> (' + (cell.ix + dx) + ',' + (cell.iy + dy) + ')');
+            return this.board.move(cell.ix, cell.iy, cell.ix + dx, cell.iy + dy);
         }).bind(this);
         cell.stepAndRefresh = (function() {
             this.stepAndRefresh();
         }).bind(this);
-        this.addChild(cell);
+        cell.stepAndRefreshUntilStable = (function() {
+            this.moved = true;
+            this.stepAndRefreshUntilStable();
+        }).bind(this);
+        cell.canMove = (function() {
+            return this.canMove();
+        }).bind(this);
+
         return cell;
+    },
+    canMove: function() {
+        return this.board.getTurn() > 0 && this.board.getStep() == 0;
     },
     cellTouchEventListener: function(sender, type) { // this는 각 cell
         switch (type) {
             case ccui.Widget.TOUCH_BEGAN:
                 {
-                    this.handleTouchMoved = true;
-                    var touchPoint = sender.getTouchBeganPosition();
-                    cc.log('Began - (' + this.ix + ',' + this.iy + ') - ' + touchPoint.x + ' ' + touchPoint.y);
+                    if (this.canMove()) {
+                        this.handleTouchMoved = true;
+                        var touchPoint = sender.getTouchBeganPosition();
+                        cc.log('Began - (' + this.ix + ',' + this.iy + ') - ' + touchPoint.x + ' ' + touchPoint.y);
+                    }
                     break;
                 }
             case ccui.Widget.TOUCH_MOVED:
                 {
-                    if (this.handleTouchMoved)
-                    {
+                    if (this.canMove() && this.handleTouchMoved) {
                         let touchBeganPoint = sender.getTouchBeganPosition();
                         let touchPoint = sender.getTouchMovePosition();
                         let d = cc.math.vec3Sub(touchPoint, touchBeganPoint);
                         let dLen = Math.sqrt(d.x * d.x + d.y * d.y);
-                        if (dLen > 15) {
+                        if (dLen > 10) {
                             let ang = Math.atan2(d.y, d.x);
-                            let halfAng = Math.PI / 20;
+                            let halfAng = Math.PI / 10;
                             if (-halfAng < ang && ang < halfAng) {
                                 // 오른쪽으로 드래그
                                 cc.log('[' + this.ix + ',' + this.iy + '] RIGHT');
                                 this.handleTouchMoved = false;
                                 let r = this.moveTo(1, 0);
                                 cc.log('move result = ' + r);
-                                this.stepAndRefresh();
+                                this.stepAndRefreshUntilStable();
                             } else if (Math.PI / 2 - halfAng < ang && ang < Math.PI / 2 + halfAng) {
                                 // 윗쪽으로 드래그
                                 cc.log('[' + this.ix + ',' + this.iy + '] UP');
                                 this.handleTouchMoved = false;
                                 let r = this.moveTo(0, -1);
                                 cc.log('move result = ' + r);
-                                this.stepAndRefresh();
+                                this.stepAndRefreshUntilStable();
                             } else if (Math.PI - halfAng < ang || ang < -Math.PI + halfAng) {
                                 // 왼쪽으로 드래그
                                 cc.log('[' + this.ix + ',' + this.iy + '] LEFT');
                                 this.handleTouchMoved = false;
                                 let r = this.moveTo(-1, 0);
                                 cc.log('move result = ' + r);
-                                this.stepAndRefresh();
+                                this.stepAndRefreshUntilStable();
                             } else if (-Math.PI / 2 - halfAng < ang && ang < -Math.PI / 2 + halfAng) {
                                 // 아랫쪽으로 드래그
                                 cc.log('[' + this.ix + ',' + this.iy + '] DOWN');
                                 this.handleTouchMoved = false;
                                 let r = this.moveTo(0, 1);
                                 cc.log('move result = ' + r);
-                                this.stepAndRefresh();
+                                this.stepAndRefreshUntilStable();
                             }
                         }
-                        cc.log('Moved - (' + this.ix + ',' + this.iy + ') - ' +
-                            touchPoint.x + ' ' + touchPoint.y + ' '
-                            //d.x + ' ' + d.y + ' ' +
-                            //dLen
-                        );
                     }
                     break;
                 }
             case ccui.Widget.TOUCH_ENDED:
-                cc.log('Touched - (' + this.ix + ',' + this.iy + ')');
+                //cc.log('Touched - (' + this.ix + ',' + this.iy + ')');
                 break;
             case ccui.Widget.TOUCH_CANCELED:
-                cc.log('Canceled - (' + this.ix + ',' + this.iy + ')');
+                //cc.log('Canceled - (' + this.ix + ',' + this.iy + ')');
                 break;
             default:
                 break;
